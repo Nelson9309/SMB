f2e25982784b523a426778d8b0cc17d2293523d9
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-11-15 00:55:46 +0300

    ывыв
    ывыв

--- js/main.js
+++ js/main.js
@@ -9,3 +9,35 @@ window.addEventListener('scroll', () =>{
 		document.querySelector('.header-dropdown').classList.remove('menutop');
 	}
 });
+
+$(function(){
+	$('.header__menu-res, .header-dropdown__inner').on('mouseover', function(){
+		$('.header-dropdown').removeClass('hide');
+		$('.header-dropdown').addClass('active');
+	});
+	$('.header__menu-other').on('mouseover', function(){
+		$('.header-dropdown').removeClass('active');
+	});
+	$('.header-dropdown').on('mouseleave', function(){
+		$('.header-dropdown').removeClass('active');
+	});
+
+	$('.header__callback').magnificPopup({
+		type:'inline',
+		midClick: true,
+		showCloseBtn: true,
+		mainClass: 'mfp-with-zoom',
+		zoom: {
+			enabled: true,
+			duration: 300,
+			easing: 'ease-in-out',
+			opener: function(openerElement) {
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+	});
+	$('.scroll').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
